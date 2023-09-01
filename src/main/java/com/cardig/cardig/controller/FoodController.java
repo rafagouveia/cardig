feade6a1fba087d9817d14bc1122eef9a8ba0236
Rafael Gomes <rafaelgomes@yupistudios.com>
2023-09-01 02:21:46 -0300

    Append others methods on controller

--- src/main/java/com/cardig/cardig/controller/FoodController.java
+++ src/main/java/com/cardig/cardig/controller/FoodController.java
@@ -14,15 +14,45 @@ import java.util.List;
 public class FoodController {
     @Autowired
     private FoodRepository repository;
+
+    @GetMapping("/{id}")
+    public FoodResponseDTO getFood(@PathVariable Long id){
+
+        Food food = repository.findById(id).orElse(null);
+        if (food == null){
+            return null;
+        }
+        return new FoodResponseDTO(food);
+    }
+
     @GetMapping
     public List<FoodResponseDTO> getAll(){
 
         return repository.findAll().stream().map(FoodResponseDTO::new).toList();
     }
+
     @PostMapping
     public void saveFood(@RequestBody FoodRequestDTO  data){
         Food foodData = new Food(data);
         repository.save(foodData);
         return;
+    }
+
+    @PutMapping("/{id}")
+    public void updateFood(@RequestBody FoodRequestDTO data, @PathVariable Long id){
+        Food foodData = new Food(data);
+        repository.findById(id).map(food -> {
+            food.setTitle(foodData.getTitle());
+            food.setImage(foodData.getImage());
+            food.setPrice(foodData.getPrice());
+            return repository.save(food);
+        }).orElseGet(() -> {
+            return repository.save(foodData);
+        });
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFood(@PathVariable Long id){
+        repository.deleteById(id);
     }
 }
--- src/main/java/com/cardig/cardig/food/Food.java
+++ src/main/java/com/cardig/cardig/food/Food.java
@@ -1,16 +1,12 @@
 package com.cardig.cardig.food;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import java.math.BigInteger;
+import lombok.*;
 
 @Table(name = "foods")
 @Entity(name = "foods")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
